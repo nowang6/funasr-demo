7511b1c96982bcca598258ab7880b8853cdfc523
nowang6 <norman6.wang@gmail.com>
2025-11-18 14:57:50 +0800

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from funasr import AutoModel
+# paraformer-zh is a multi-functional asr model
+# use vad, punc, spk or not as you need
+model = AutoModel(model="paraformer-zh", model_revision="v2.0.4",
+                  vad_model="fsmn-vad", vad_model_revision="v2.0.4",
+                  punc_model="ct-punc-c", punc_model_revision="v2.0.4",
+                  # spk_model="cam++", spk_model_revision="v2.0.2",
+                  )
+res = model.generate(input=f"创建警单.wav",
+                     batch_size_s=300, 
+                     hotword='警单')
+print(res)
+
